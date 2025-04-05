2c58c28df19dd5fdba12edd2296131ad2b452259
Gokdenizb <148850933+Gokdenizb@users.noreply.github.com>
2025-04-05 21:52:59 +0300

    Add files via upload

---
+++ Tcp-Server/ToyotaProject_2025/README.md
@@ -0,0 +1,4 @@
+# ToyotaProject_2025
+This project is part of the Toyota/32bit employment program for students at Sakarya University.
+
+More details will be shared as the project progresses.
---
+++ Tcp-Server/ToyotaProject_2025/Tcp-Server/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    public static void main(String[] args) {
+
+        final Logger logger = LogManager.getLogger(Main.class);
+
+
+        try{
+            ConfigLoader configLoader = ConfigLoader.loadFromConfigFile("/Users/gokdenizbayik/IdeaProjects/ToyotaProject_2025/Tcp-Server/src/main/java/org/example/config.json");
+            CurrencyDataGenerator.configure(configLoader);
+            new TcpServer(8081 , configLoader).startServer();
+        } catch (Exception e){
+            logger.error("An error occurred: {}", e.getMessage());
+        }
+    }
+}
