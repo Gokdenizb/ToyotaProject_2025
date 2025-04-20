b94e537dfaa494cc3799bda8f8682cfea067604c
Gokdenizb <148850933+Gokdenizb@users.noreply.github.com>
2025-04-20 23:12:41 +0300

    Rest data provider updated to meet more requirements: -Now application read initial data from config.json file. -Application returns formatted data via end-point "/api/rate/'subscription string'". -Application returns json data with data transfer object. -Application doesn't allow bid data being greater than ask data. -Further changes will be added.

--- REST_API/src/main/java/com/example/rest_api/controller/ExchangeRateController.java
+++ REST_API/src/main/java/com/example/rest_api/controller/ExchangeRateController.java
@@ -1,20 +1,25 @@
 package com.example.rest_api.controller;
 
-import com.example.rest_api.Service.TcpClientService;
-import org.springframework.web.bind.annotation.*;
+import com.example.rest_api.service.ExchangeRateSimulationService;
+import com.example.rest_api.service.ExchangeRateSimulationService.ExchangeRateDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @RequestMapping("/api/rates")
 public class ExchangeRateController {
-    private final TcpClientService tcpClientService;
 
-    public ExchangeRateController(TcpClientService tcpClientService){
-        this.tcpClientService = tcpClientService;
+    private final ExchangeRateSimulationService simulationService;
+
+    public ExchangeRateController(ExchangeRateSimulationService exchangeRateSimulationService){
+        this.simulationService = exchangeRateSimulationService;
     }
 
     @GetMapping("/{currencyPair}")
-    public String getExchangeRates(@PathVariable String currencyPair){
-        return tcpClientService.getExchangeRates(currencyPair);
+    public ExchangeRateDTO getExchangeRates(@PathVariable String currencyPair){
+        return simulationService.getCurrentRate(currencyPair);
     }
 }
